2399b7d34bd220281dec25e42f65c2b6be61967f
mrzhuzhe <466648706@qq.com>
2025-05-14 23:18:21 +0800

    uart

---
+++ Examples/UART/main.c
@@ -0,0 +1,52 @@
+#include <stm32f1xx_hal.h>
+#include "oled.h"
+
+// STM32VL-Discovery green led - PC9
+#define LED_PORT                GPIOC
+#define LED_PIN                 GPIO_PIN_13
+#define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
+
+void SysTick_Handler(void)
+{
+    HAL_IncTick();
+
+    // 1 Hz blinking
+    if ((HAL_GetTick() % 5000) == 0)
+    {
+        HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+    }
+}
+
+void initGPIO()
+{
+    GPIO_InitTypeDef GPIO_Config;
+
+    GPIO_Config.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_Config.Pull = GPIO_NOPULL;
+    GPIO_Config.Speed = GPIO_SPEED_FREQ_HIGH;
+
+    GPIO_Config.Pin = LED_PIN;
+
+    LED_PORT_CLK_ENABLE();
+    HAL_GPIO_Init(LED_PORT, &GPIO_Config);
+}
+
+int main(void)
+{
+    HAL_Init();
+    initGPIO();
+    // 1kHz ticks
+    HAL_SYSTICK_Config(SystemCoreClock / 1000);
+
+    OLED_Init();
+	OLED_ShowChar(1, 1, 'A');
+	OLED_ShowString(1, 3, "HelloWorld!");
+	OLED_ShowNum(2, 1, 12345, 5);
+	OLED_ShowSignedNum(2, 7, -66, 4);
+	OLED_ShowHexNum(3, 1, 0xAA55, 4);
+	OLED_ShowBinNum(4, 1, 0xAA55, 16);
+
+    while(1);
+
+    return 0;
+}
