8c6272097af77850a0bdd34622957fee5f2a35db
mrzhuzhe <466648706@qq.com>
2025-05-14 23:16:47 +0800

    oled

--- Examples/OLED/main.c
+++ Examples/OLED/main.c
@@ -1,18 +1,17 @@
-#if defined STM32F1
-    #include <stm32f1xx_hal.h>
+#include <stm32f1xx_hal.h>
+#include "oled.h"
 
-    // STM32VL-Discovery green led - PC9
-    #define LED_PORT                GPIOC
-    #define LED_PIN                 GPIO_PIN_13
-    #define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
-#endif
+// STM32VL-Discovery green led - PC9
+#define LED_PORT                GPIOC
+#define LED_PIN                 GPIO_PIN_13
+#define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
 
 void SysTick_Handler(void)
 {
     HAL_IncTick();
 
     // 1 Hz blinking
-    if ((HAL_GetTick() % 500) == 0)
+    if ((HAL_GetTick() % 5000) == 0)
     {
         HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
     }
@@ -38,6 +37,14 @@ int main(void)
     initGPIO();
     // 1kHz ticks
     HAL_SYSTICK_Config(SystemCoreClock / 1000);
+
+    OLED_Init();
+	OLED_ShowChar(1, 1, 'A');
+	OLED_ShowString(1, 3, "HelloWorld!");
+	OLED_ShowNum(2, 1, 12345, 5);
+	OLED_ShowSignedNum(2, 7, -66, 4);
+	OLED_ShowHexNum(3, 1, 0xAA55, 4);
+	OLED_ShowBinNum(4, 1, 0xAA55, 16);
 
     while(1);
 
