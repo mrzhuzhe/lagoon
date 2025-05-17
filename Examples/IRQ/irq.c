7d74a105a70de77bb3bdc33c719e75da05df66c4
mrzhuzhe <466648706@qq.com>
2025-05-17 23:42:39 +0800

    irq

--- Examples/IRQ/irq.c
+++ Examples/IRQ/irq.c
@@ -5,7 +5,7 @@ void init_irq() {
 
     GPIO_InitTypeDef GPIO_Config = {0};
 
-    GPIO_Config.Mode = GPIO_MODE_INPUT;
+    GPIO_Config.Mode = GPIO_MODE_IT_RISING_FALLING | GPIO_MODE_INPUT;
     //GPIO_Config.Mode = GPIO_MODE_AF_OD;
     GPIO_Config.Pull = GPIO_PULLUP;
     //GPIO_Config.Pull = GPIO_NOPULL;
@@ -23,27 +23,31 @@ void init_irq() {
 
     GPIO_Config.Pin =  GPIO_PIN_13 | GPIO_PIN_15;
     HAL_GPIO_Init(GPIOC, &GPIO_Config);
-
-    GPIO_Config.Pin = GPIO_PIN_1;
-    GPIO_Config.Mode = GPIO_MODE_IT_RISING | GPIO_MODE_INPUT;
-    GPIO_Config.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOB, &GPIO_Config);
     
     /* EXTI interrupt init*/
     HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(EXTI1_IRQn);
-
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, (GPIO_PinState)1);
-
 }
 
 void EXTI1_IRQHandler(void) {
-   HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_1);
+   //HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_1);
+   HAL_GPIO_EXTI_Callback(GPIO_PIN_15);
 }
 
-
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-
-{ 
-    OLED_ShowNum(4, 4, HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1), 4);
+unsigned io_code = 0b11111111;
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){ 
+    unsigned cur_code = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11) << 7;
+    //unsigned cur_code = 0;
+    cur_code += HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1) << 6;
+    cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7) << 5;
+    cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) << 4;
+    cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3) << 3;
+    cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1) << 2;
+    cur_code += HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) << 1;
+    cur_code += HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
+    if (cur_code != io_code){
+        //HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, (GPIO_PinState)cur_code);
+        io_code = cur_code;
+        OLED_ShowBinNum(2, 1, io_code, 8);
+    }
 }
--- Examples/IRQ/main.c
+++ Examples/IRQ/main.c
@@ -2,24 +2,24 @@
 #include "oled.h"
 #include "irq.h"
 
-unsigned io_code = 0b11111111;
+
 void SysTick_Handler(void)
 {
     HAL_IncTick();
-    unsigned cur_code = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11) << 7;
-    //unsigned cur_code = 0;
-    cur_code += HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1) << 6;
-    cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7) << 5;
-    cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) << 4;
-    cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3) << 3;
-    cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1) << 2;
-    cur_code += HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) << 1;
-    cur_code += HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
-    if (cur_code != io_code){
-        //HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, (GPIO_PinState)cur_code);
-        io_code = cur_code;
-        OLED_ShowBinNum(2, 1, io_code, 8);
-    }
+    // unsigned cur_code = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11) << 7;
+    // //unsigned cur_code = 0;
+    // cur_code += HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1) << 6;
+    // cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_7) << 5;
+    // cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_5) << 4;
+    // cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3) << 3;
+    // cur_code += HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1) << 2;
+    // cur_code += HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) << 1;
+    // cur_code += HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
+    // if (cur_code != io_code){
+    //     //HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, (GPIO_PinState)cur_code);
+    //     io_code = cur_code;
+    //     OLED_ShowBinNum(2, 1, io_code, 8);
+    // }
 }
 
 int main(void)
@@ -32,7 +32,7 @@ int main(void)
     OLED_Init();
 	OLED_ShowChar(1, 1, 'A');
 	OLED_ShowString(1, 3, "Fuck you!");
-    OLED_ShowBinNum(2, 1, io_code, 8);
+    OLED_ShowBinNum(2, 1, 0b11111111, 8);
     while(1);
 
     return 0;
