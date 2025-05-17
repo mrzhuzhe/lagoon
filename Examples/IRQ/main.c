1b24e2ff77d19597d849062aaf68f8d3365e1f4f
mrzhuzhe <466648706@qq.com>
2025-05-17 19:44:22 +0800

    wrong

--- Examples/IRQ/main.c
+++ Examples/IRQ/main.c
@@ -2,20 +2,16 @@
 #include "oled.h"
 #include "irq.h"
 
-GPIO_PinState io_code = 0;
+GPIO_PinState io_code = 1;
 void SysTick_Handler(void)
 {
     HAL_IncTick();
     GPIO_PinState cur_code = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11);
     if (cur_code != io_code){
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, cur_code);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, (GPIO_PinState)cur_code);
         io_code = cur_code;
-        OLED_ShowNum(2, 1, io_code, 5);   
+        OLED_ShowBinNum(2, 1, io_code, 8);
     }
-    // if ((HAL_GetTick() % 1000) == 0)
-    // {
-    //    
-    // }
 }
 
 int main(void)
@@ -27,8 +23,8 @@ int main(void)
 
     OLED_Init();
 	OLED_ShowChar(1, 1, 'A');
-	OLED_ShowString(1, 3, "Fuck you!");	
-
+	OLED_ShowString(1, 3, "Fuck you!");
+    OLED_ShowBinNum(2, 1, io_code, 8);
     while(1);
 
     return 0;
