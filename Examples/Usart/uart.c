7b50887f46f2f3059a110cf7a084eaf7091e5606
mrzhuzhe <466648706@qq.com>
2025-05-18 15:58:25 +0800

    uart init

--- Examples/Usart/uart.c
+++ Examples/Usart/uart.c
@@ -1,158 +1,47 @@
 #include <stm32f1xx_hal.h>
+#include "oled.h"
 
+USART_HandleTypeDef UsartHandle;
 
-uint8_t Serial_RxData;
-uint8_t Serial_RxFlag;
+void Uart_init(){
 
-void Serial_Init(void)
-{
 	
-	GPIO_InitTypeDef GPIO_InitStructure;
-	USART_InitTypeDef USART_InitStructure;
-	NVIC_InitTypeDef NVIC_InitStructure;
-	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	// 输出引脚TX
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 ;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	// 接收引脚 RX
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 ;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
-	
-	USART_InitStructure.USART_BaudRate = 9600; // 波特率，直接写就行，内部会自动帮忙计算9600对应的分频系数，然后写入BRR寄存器
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // 硬件流控
-	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx; // 或上接收模式即可
-	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	
-	USART_Init(USART1, &USART_InitStructure);
-	
-	// 打开USART中断
-	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-	
-	// RXNE标志位 一旦置1了，就会向NVIC申请中断
-	
-	// 配置NVIC
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	
-	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-	
-	NVIC_Init(&NVIC_InitStructure);
-	
-	
-	USART_Cmd(USART1, ENABLE);
+    GPIO_InitTypeDef  GPIO_InitStruct;
+  
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_USART1_CLK_ENABLE(); 
+    
+    // stty -F /dev/ttyUSB0 115200 cs8 -cstopb -parenb -echo -icanon -onlcr
+    GPIO_InitStruct.Pin       = GPIO_PIN_9;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull      = GPIO_PULLUP;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_MEDIUM;
+
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_10;
+    GPIO_InitStruct.Mode      = GPIO_MODE_INPUT;
+
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+        
+    // HAL_NVIC_SetPriority(USART1_IRQn, 0, 1);
+    // HAL_NVIC_EnableIRQ(USART1_IRQn);
+
+	UsartHandle.Instance        = USART1;
+
+    UsartHandle.Init.BaudRate   = 115200;
+    UsartHandle.Init.WordLength = USART_WORDLENGTH_8B;
+    UsartHandle.Init.StopBits   = USART_STOPBITS_1;
+    UsartHandle.Init.Parity     = USART_PARITY_NONE;
+    //UsartHandle.Init.HwFlowCtl  = USART_HWCONTROL_NONE;
+    UsartHandle.Init.Mode       = USART_MODE_TX_RX;
+    HAL_USART_Init(&UsartHandle);
+
+    uint8_t Test[] = "Hello World !!!\r\n"; 
+    HAL_USART_Transmit(&UsartHandle, Test, sizeof(Test), 10);
+
 }
 
-
-
-void Serial_SendByte(uint8_t Byte)
-{
-	USART_SendData(USART1, Byte);
-	while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-	// 在库函数的状态寄存器中有一句话：对USART_DR的写操作，将该位清零。
-	// 所以当我们下一次在SendData时，它会自动清零，那么我们就不需要手动清零了
-}
-
-
-void Serial_SendArray(uint8_t *Array, uint16_t Length)
-{
-	uint16_t i;
-	for(i = 0; i < Length; i ++)
-	{
-		Serial_SendByte(Array[i]);
-	}
-}
-
-void Serial_SendString(char *String)
-{
-	
-	uint8_t i;
-	for(i = 0; String[i] != '\0'; i ++)
-	{
-		Serial_SendByte(String[i]);
-	}
-	
-}
-
-uint32_t Serial_Pow(uint32_t X, uint32_t Y)
-{
-	uint32_t Result = 1;
-	while(Y --)
-	{
-		Result *= X;
-	}
-	return Result;
-}
-
-void Serial_SendNumber(uint32_t Number, uint8_t Length)
-{
-
-	uint8_t i;
-	char offset = '0';
-
-	for(i = 0; i < Length; i ++)
-	{
-		Serial_SendByte(Number / Serial_Pow(10, Length - 1 - i) % 10 + offset);
-	}
-	
-}
-
-// 重写 stdio.h 中的 fputc 函数，fputc是printf函数的底层，这里相当于将printf函数重定向到了串口
-int fputc(int ch, FILE *f)
-{
-	Serial_SendByte(ch);
-	return ch;
-}
-
-
-// 封装 串口使用sprintf 的过程//////相关知识点：C语言可变参数列表
-void Serial_Printf(char *format, ...)
-{
-	char String[100];
-	va_list arg;
-	va_start(arg, format);
-	vsprintf(String, format, arg);
-	va_end(arg);
-	Serial_SendString(String);
-}
-
-
-uint8_t Serial_GetRxFlag(void)
-{
-	if(Serial_RxFlag == 1)
-	{
-		Serial_RxFlag = 0;
-		return 1;
-	}
-	return 0;
-}
-
-
-uint8_t Serial_GetRxData(void)
-{
-	return Serial_RxData;
-}
-
-void USART1_IRQHandler(void)
-{
-	if(USART_GetITStatus(USART1, USART_IT_RXNE) == SET)
-	{
-		// 可以读取DR
-		Serial_RxData = USART_ReceiveData(USART1);
-		Serial_RxFlag = 1;
-		USART_ClearITPendingBit(USART1, USART_IT_RXNE);
-	}
-	
-}
+// void HAL_USART_RxCpltCallback(USART_HandleTypeDef *husart) {
+//    OLED_ShowBinNum(2, 1, 123, 8); 
+// }
